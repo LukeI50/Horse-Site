dec333c86e37e9b73d169567a6f98de050e0009e
James Vanstone <jv228882@falmouth.ac.uk>
2024-12-18 11:56:29 +0000

    add question view and questionnaire view

--- questions/urls.py
+++ questions/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.questionnaire_view.as_view(), name="questionnaire view")
+    path("<int:questionnaire_id>", views.questionnaire_view.as_view(), name="questionnaire view"),
+    path("<int:questionnaire_id>/<int:question_id>", views.question_view.as_view(), name="question view"),
     ]

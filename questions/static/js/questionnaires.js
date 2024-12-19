40d276c4cd72f942976027b026db09fa02e51b5c
LukeI50 <180131099+LukeI50@users.noreply.github.com>
2024-12-19 02:05:15 +0000

    import: more of the utility functions from LukeI50's emmet snippet for js UtilityFunctions class

--- questions/static/js/questionnaires.js
+++ questions/static/js/questionnaires.js
@@ -1,5 +1,39 @@
 // LukeI50's Emmet JS Utility Functions
 class UtilityFunctions{
+    elementCreate(element_name, class_list=[], text="", action="", method="", href="", attributes_obj={}) {
+        const newElement = document.createElement(element_name);
+
+        if (Array.isArray(class_list)) {
+            newElement.classList.add(...class_list);
+        } else {
+            newElement.classList.add(class_list);
+        }
+
+        newElement.innerText = text;
+
+        if (attributes_obj && Object.keys(attributes_obj).length > 0) {
+            Object.entries(attributes_obj).forEach(([key, value]) => {
+                newElement.setAttribute(key, value);
+            });
+        }
+
+        return newElement
+    };
+
+
+    cookieCreate(cookie_name, cookie_value) {
+        document.cookie = `${cookie_name}=` + `${cookie_value}` + "; path=/; Secure; SameSite=None;"
+    };
+
+
+    cookieCreateFromObj(name_and_value) {
+        let cookieString = "";
+        Object.entries(name_and_value).forEach(([key, value]) => {
+            cookieString += `${key}=${value}; `;
+        });
+        document.cookie = `${cookieString}` + "path=/; Secure; SameSite=None;"
+        
+    }
     hideElement(targetElement) {
         targetElement.classList.add('d-none');
     }
@@ -9,26 +43,23 @@ class UtilityFunctions{
 };
 
 
-
-
 document.addEventListener("DOMContentLoaded", function() {
-
-
-
-
 
     // Check script and dom are loaded
     console.log("Questionnaire Script Loaded")
 
     // Establish Variables
     const questionCarousel = document.getElementById('questionCarousel');
-    const carouselsNext = document.getElementById('questionCarouselNext')
-    const carouselsPrevious = document.getElementById('questionCarouselPrevious')
+    const carouselsNext = document.getElementById('questionCarouselNext');
+    const carouselsPrevious = document.getElementById('questionCarouselPrevious');
 
-    const allAnswers = document.getElementsByClassName('answer-options')
+    const allAnswers = document.getElementsByClassName('answer-options');
     const carouselItems = document.getElementsByClassName('carousel-item');
+    const carouselControlsContainer = document.getElementById('question-carousel-controls');
 
-    const submitButton = document.createElement('button')
+
+    // Create instance of UtilityFunctions class
+    const utilityFunctions = new UtilityFunctions(); 
 
     console.log(carouselItems)
 
@@ -42,10 +73,8 @@ document.addEventListener("DOMContentLoaded", function() {
     };
 
     // Hide previous button initially
-    carouselsPrevious.classList.add('d-none');
+    utilityFunctions.hideElement(carouselsPrevious);
 
-    // Create instance of UtilityFunctions class
-    const utilityFunctions = new UtilityFunctions(); 
 
 
     questionCarousel.addEventListener('slide.bs.carousel', (e) => {
@@ -55,16 +84,23 @@ document.addEventListener("DOMContentLoaded", function() {
             utilityFunctions.showElement(carouselsPrevious);
         }
 
-        if (e.to === carouselItems.length) {
+        if (e.to === carouselItems.length - 1) {
             utilityFunctions.hideElement(carouselsNext);
+            finish_button = utilityFunctions.elementCreate(
+                "button",
+                ["btn", "btn-primary"],
+                "Get Restults",
+            )
+            carouselControlsContainer.appendChild(finish_button);
+
         } else if (carouselsNext.classList.contains('d-none')) {
-            utilityFunctions.hideElement(carouselsNext);
+            utilityFunctions.showElement(carouselsNext);
+            finish_button.remove();
         }
 
 
         // currentQuestion = event.relatedTarget
         currentQuestion = e.relatedTarget.getAttribute('data-question-id');
-        console.log(currentQuestion);
 
         // get answer weighting
 

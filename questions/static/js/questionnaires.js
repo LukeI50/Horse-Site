5f8c8fb18b678c5b4f7295d0836bed6cc09b773d
LukeI50 <180131099+LukeI50@users.noreply.github.com>
2024-12-19 00:45:21 +0000

    add: utility functions emmet snippet shortened from 12 to 2 functions. add show hide logic for next/previous buttons

--- questions/static/js/questionnaires.js
+++ questions/static/js/questionnaires.js
@@ -1,4 +1,21 @@
+// LukeI50's Emmet JS Utility Functions
+class UtilityFunctions{
+    hideElement(targetElement) {
+        targetElement.classList.add('d-none');
+    }
+    showElement(targetElement) {
+        targetElement.classList.remove('d-none');
+    }
+};
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
+
+
+
+
 
     // Check script and dom are loaded
     console.log("Questionnaire Script Loaded")
@@ -6,10 +23,12 @@ document.addEventListener("DOMContentLoaded", function() {
     // Establish Variables
     const questionCarousel = document.getElementById('questionCarousel');
     const carouselsNext = document.getElementById('questionCarouselNext')
-    const carouselPrevious = document.getElementById('questionCarouselPrevious')
+    const carouselsPrevious = document.getElementById('questionCarouselPrevious')
 
     const allAnswers = document.getElementsByClassName('answer-options')
     const carouselItems = document.getElementsByClassName('carousel-item');
+
+    const submitButton = document.createElement('button')
 
     console.log(carouselItems)
 
@@ -18,16 +37,40 @@ document.addEventListener("DOMContentLoaded", function() {
     // Set Current question on load.
     for (let carouselItem of carouselItems) {
         if (carouselItem.classList.contains('active')) {
-            let currentQuestion = carouselItem.getAttribute('data-question-id');
+            currentQuestion = carouselItem.getAttribute('data-question-id');
         };
     };
 
+    // Hide previous button initially
+    carouselsPrevious.classList.add('d-none');
+
+    // Create instance of UtilityFunctions class
+    const utilityFunctions = new UtilityFunctions(); 
+
 
     questionCarousel.addEventListener('slide.bs.carousel', (e) => {
-        console.log(event.relatedTarget);
+        if (e.to === 0) {
+            utilityFunctions.hideElement(carouselsPrevious);
+        } else if (carouselsPrevious.classList.contains('d-none')) {
+            utilityFunctions.showElement(carouselsPrevious);
+        }
+
+        if (e.to === carouselItems.length) {
+            utilityFunctions.hideElement(carouselsNext);
+        } else if (carouselsNext.classList.contains('d-none')) {
+            utilityFunctions.hideElement(carouselsNext);
+        }
+
+
         // currentQuestion = event.relatedTarget
         currentQuestion = e.relatedTarget.getAttribute('data-question-id');
         console.log(currentQuestion);
+
+        // get answer weighting
+
     })
 
+
+
 });
+

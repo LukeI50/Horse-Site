64b2afe65558af525e5d47595e18810c42bdd8a6
LukeI50 <180131099+LukeI50@users.noreply.github.com>
2024-12-19 00:06:25 +0000

    add: question id from carousel

--- questions/static/js/questionnaires.js
+++ questions/static/js/questionnaires.js
@@ -4,13 +4,30 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("Questionnaire Script Loaded")
 
     // Establish Variables
-    const questionCarousel = document.querySelector('#questionCarousel');
+    const questionCarousel = document.getElementById('questionCarousel');
+    const carouselsNext = document.getElementById('questionCarouselNext')
+    const carouselPrevious = document.getElementById('questionCarouselPrevious')
 
-    const carousel = new bootstrap.Carousel(questionCarousel, {
-        touch: true
-    });
+    const allAnswers = document.getElementsByClassName('answer-options')
+    const carouselItems = document.getElementsByClassName('carousel-item');
 
-    
+    console.log(carouselItems)
+
+    let currentQuestion = 0;
+
+    // Set Current question on load.
+    for (let carouselItem of carouselItems) {
+        if (carouselItem.classList.contains('active')) {
+            let currentQuestion = carouselItem.getAttribute('data-question-id');
+        };
+    };
 
 
-})
+    questionCarousel.addEventListener('slide.bs.carousel', (e) => {
+        console.log(event.relatedTarget);
+        // currentQuestion = event.relatedTarget
+        currentQuestion = e.relatedTarget.getAttribute('data-question-id');
+        console.log(currentQuestion);
+    })
+
+});
